eb43b5d7f835c5f5e78c33d5949694b4d3f41c79
travisprol <travisjohnprol@gmail.com>
2020-06-04 13:59:25 -0400

    initial owner and cat classes

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  # attr_accessor :species
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
